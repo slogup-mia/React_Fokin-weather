bb68815aefe5ad31b0c21b7ffa28d5d9dcfa0a77
unknown <sw.onandon@gmail.com>
2020-07-08 16:18:30 +0900

    0708 : fin Chapter #2.3 Background Gradient

--- App.js
+++ App.js
@@ -15,7 +15,6 @@ export default class extends React.Component {
 
   getWeather = async (latitude, longitude) => {
     const {data} = await axios.get(
-
         `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${API_KEY}`)
     console.log(data)
     this.setState({isLoading:false, temp:data.main.temp, condition:data.weather[0].main})
@@ -25,18 +24,12 @@ export default class extends React.Component {
   getLocation = async () => {
     try {
       await Location.requestPermissionsAsync();
-
       const { coords : {latitude, longitude} } = await Location.getCurrentPositionAsync();
-      // console.log(latitude, longitude)
-
-      this.setState({isLoading: false})
       this.getWeather(latitude, longitude)
     } catch (error) {
       Alert.alert("Can't find you", 'So Sad');
     }
   };
-  // Error : LOCATION permission is required to do this operation.
-    // 모바일유저의 현재위치 데이터 제공 허가가 있어야 한다.
 
   componentDidMount() {
     this.getLocation();
--- weather.js
+++ weather.js
@@ -1,32 +1,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, StatusBar} from 'react-native';
 
 import { MaterialCommunityIcons } from "@expo/vector-icons";
-// https://docs.expo.io/guides/icons/
-// https://icons.expo.fyi/
-// icon family = [ MaterialCommunityIcons, AntDesign, Entypo, FontAwesome, Ionicons.. 등 매우 많다. ]
-// import { iconFamilyName } from "@expo/vector-icons"; 으로 임포트해서 사용
+    // https://docs.expo.io/guides/icons/
+    // https://icons.expo.fyi/
+    // icon family = [ MaterialCommunityIcons, AntDesign, Entypo, FontAwesome, Ionicons.. 등 매우 많다. ]
+    // import { iconFamilyName } from "@expo/vector-icons"; 으로 임포트해서 사용
+import { LinearGradient } from 'expo-linear-gradient';
+    // expo install expo-linear-gradient
+    // https://docs.expo.io/versions/latest/sdk/linear-gradient/
+    // 그라디언트 만들기 :
 
-
+const weatherOptions = {
+    Thunderstorm : { iconName : 'weather-lightning', colors: ['#636363','#a2ab58']},
+    Drizzle : { iconName : 'weather-hail', colors: ['#74ebd5','#ACB6E5']},
+    Rain : { iconName : 'weather-rainy', colors: ['#36D1DC','#5B86E5']},
+    Snow : { iconName : 'weather-snowy', colors: ['#E0EAFC','#CFDEF3']},
+    Mist : { iconName : 'weather-fog', colors: ['#ada996','#f2f2f2','#dbdbdb','#eaeaea']},
+    Smoke : { iconName : 'weather-fog', colors: ['#616161','#9bc5c3']},
+    Haze : { iconName : 'weather-hazy', colors: ['#77A1D3','#79CBCA','#E684AE']},
+    Dust : { iconName : 'weather-hazy', colors: ['#649173','#DBD5A4']},
+    Fog : { iconName : 'weather-fog', colors: ['#283048','#859398']},
+    Sand : { iconName : 'weather-hazy', colors: ['#3E5151','#DECBA4']},
+    Ash : { iconName : 'weather-hazy', colors: ['#606c88','#3f4c6b']},
+    Squall : { iconName : 'weather-tornado', colors: ['#2980B9','#6DD5FA','#ffffff']},
+    Tornado : { iconName : 'weather-hurricane', colors: ['#4B79A1','#283E51']},
+    Clear : { iconName : 'weather-sunny', colors: ['#2980B9','#6DD5FA','#ffffff']},
+    Clouds : { iconName : 'weather-cloudy', colors: ['#77A1D3','#79CBCA']},
+}
 
 export default function Weather({temp, condition}){
-    return <View style={styles.container}>
-        <View style={styles.halfContainer}>
-            <MaterialCommunityIcons name="weather-rainy" size={120}/>
-            <Text style={styles.temp}>{temp}°C</Text>
-            <Text>{condition}</Text>
-        </View>
-        <View style={styles.halfContainer}>
-
-        </View>
-    </View>
+    return(
+            <LinearGradient
+                colors={weatherOptions[condition].colors}
+                style={styles.container}>
+                <StatusBar barStyle='light-content'/>
+                <View style={styles.halfContainer}>
+                    <MaterialCommunityIcons name={weatherOptions[condition].iconName} size={120} color='white'/>
+                    <Text style={styles.temp}>{temp}°C</Text>
+                    <Text style={{color: 'white'}}>{condition}</Text>
+                </View>
+                <View style={styles.halfContainer}>
+                </View>
+            </LinearGradient>
+    )
 }
 
 Weather.propTypes = {
     temp: PropTypes.number.isRequired,
     condition: PropTypes
-        .oneOf(['Thunderstorm', 'Drizzle', 'Rain', 'Snow', 'Mist','Smoke','Haze','Dust','Fog','Sand','Dust','Ash','Squall','Tornado', 'Clear', 'Clouds']
+        .oneOf(['Thunderstorm', 'Drizzle', 'Rain', 'Snow', 'Mist','Smoke','Haze','Dust','Fog','Sand','Ash','Squall','Tornado', 'Clear', 'Clouds']
         ).isRequired
 };
 
@@ -42,7 +66,8 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     temp:{
-        fontSize:48
+        fontSize:48,
+        color: 'white'
     }
 });
 
